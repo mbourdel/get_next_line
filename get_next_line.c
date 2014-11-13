276f5a315e940a323002a0fc8f451078ed9f3574
mbourdel <mlebourdelles@free.fr>
2014-11-13 16:33:48 +0100

    13/11/2014_16:34  just an up

--- get_next_line.c
+++ get_next_line.c
@@ -6,8 +6,37 @@
 /*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 14:35:03 by mbourdel          #+#    #+#             */
-/*   Updated: 2014/11/13 14:35:10 by mbourdel         ###   ########.fr       */
+/*   Updated: 2014/11/13 16:32:58 by mbourdel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
 
+int		get_next_line(const int fd, char **line)
+{
+	char		buffer[BUFF_SIZE];
+	static int	noline = 0;
+	int			i;
+	int			isfinish;
+
+	i = 1;
+	isfinish = 1;
+	if ((int)read(fd, buffer[0], 1) == -1)
+		return (-1);
+	while (buffer[i - 1] != '\n' && isfinish > 0)
+		isfinish = read(fd, buffer[i++], 1);
+	if (isfinish = -1)
+		return (-1);
+	i = 0;
+	while (buffer[i])
+	{
+		line[noline][i] = buffer[i];
+		i++;
+	}
+	if (isfinish > 0)
+	{
+		noline++;
+		return (1);
+	}
+	return (0);
+}
--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,13 @@
 /*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 14:48:23 by mbourdel          #+#    #+#             */
-/*   Updated: 2014/11/13 14:49:56 by mbourdel         ###   ########.fr       */
+/*   Updated: 2014/11/13 16:33:01 by mbourdel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 #define GET_NEXT_LINE_H
+#include <unistd.h>
 
 int		get_next_line(const int fd, char **line);
 
