38351bc697f000aa22b21107c8033c5e9e0dac61
mbourdel <mlebourdelles@free.fr>
2014-11-19 18:41:38 +0100

    19/11/2014_18:41  up .c .h  but segfault

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 14:35:03 by mbourdel          #+#    #+#             */
-/*   Updated: 2014/11/18 17:31:27 by mbourdel         ###   ########.fr       */
+/*   Updated: 2014/11/19 18:37:30 by mbourdel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,6 @@ t_index	ft_register_new_file(t_index index, const int fd)
 	new_file = (t_index)malloc(sizeof(t_index));
 	new_file->file[0] = fd;
 	new_file->file[1] = 0;
-	new_file->file[2] = 0;
 	new_file->nxt = index;
 	index->pvs = new_file;
 	new_file->pvs = NULL;
@@ -31,7 +30,7 @@ int		ft_is_new_file(t_index index, const int fd)
 	t_index		tmp;
 	char		c;
 
-	if (read(fd, &c, 1) == -1)
+	if (read(fd, &c, 0) == -1)
 		return (-1);
 	while (index->pvs != NULL)
 		index = index->pvs;
@@ -58,23 +57,22 @@ char	*ft_get_this_line(t_index index, char buff[BUFF_SIZE])
 {
 	int		i;
 	int		error;
-	int		charead;
 
-	i = 0;
+	i = 1;
 	error = 1;
-	charead = index->file[2];
-	while (buff[i - 1] != '\n' && buff[i- 1] && error == 1)
-		error = read(fd, buff[i++], 1);
-	index->file[1] += 1;
-	index->file[2] += i;
-	return (buff);
+	while (buff[i - 1] != '\n' && buff[i - 1] && error == 1
+			&& i < BUFF_SIZE)
+		error = read(index->file[0], &buff[i++], 1);
+	if (error == -1)
+		return (NULL);
+	return (++buff);
 }
 
 int		get_next_line(const int fd, char **line)
 {
-	static 	t_index	index = NULL;
-	int		error;
-	char	buff[BUFF_SIZE];
+	static t_index	index = NULL;
+	int				error;
+	char			buff[BUFF_SIZE];
 
 	error = 0;
 	if ((error = ft_is_new_file(index, fd)) == 1)
@@ -82,7 +80,9 @@ int		get_next_line(const int fd, char **line)
 	else if (error == -1)
 		return (-1);
 	index = ft_get_the_good_file(index, fd);
-
-
+	if ((line[index->file[1]] = ft_strdup(ft_get_this_line(index, buff)))
+			== NULL)
+		return (-1);
+	index->file[1] += 1;
 	return (1);
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbourdel <mbourdel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/11/13 14:48:23 by mbourdel          #+#    #+#             */
-/*   Updated: 2014/11/18 15:25:49 by mbourdel         ###   ########.fr       */
+/*   Updated: 2014/11/19 17:32:27 by mbourdel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,14 @@
 # define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdlib.h>
+# include "./libft/libft.h"
 # define BUFF_SIZE 999
 
 typedef struct s_file	t_file;
 
 struct					s_file
 {
-	int					file[3];
+	int					file[2];
 	t_file				*nxt;
 	t_file				*pvs;
 };
@@ -30,6 +31,7 @@ typedef t_file			*t_index;
 t_index					ft_register_new_file(t_index index, const int fd);
 t_index					ft_get_the_good_file(t_index index, const int fd);
 int						ft_is_new_file(t_index index, const int fd);
+char					*ft_get_this_line(t_index index, char buff[BUFF_SIZE]);
 int						get_next_line(const int fd, char **line);
 
 #endif
